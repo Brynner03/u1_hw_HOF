fe8529541c7ba630dd62792ddb2b833137a1fc21
Brynner Ventura <bventura03@hotmail.com>
2022-03-02 18:58:14 -0500

    Initial commit

--- solution.js
+++ solution.js
@@ -1,29 +1,53 @@
 // You must RETURN your result in each function!
 
-function printNums(nums) {
+
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
-}
+   let nums = [1,2,3,4]
+   let numa = [32,43,53]
+  
+   nums.forEach((a) => {
+     numa.unshift(nums)
+   })
+console.log(numa)
 
-function returnSums(nums) {
+
+ 
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
-}
 
-function returnTotal(objs) {
+   let newNum = []
+   nums.forEach((n, index) => {
+     newNum.push(n + index)
+   })
+   console.log(newNum)
+
+
+
+
+
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
    * @param objs => [{n:1}]
-   */
-}
+   *
+**/
+let objects = [23,42,24,55]
+const sum = objects.reduce((a,b) => {
+  return a+b
+})
 
-function printMoney(decimals) {
+
+
+
+  
+
+
   /**
    * @param decimals => [2.30, 2.40, 32.99]
    * Using .map, return an array of decimals formatted as dollars.
@@ -33,16 +57,30 @@ function printMoney(decimals) {
    * }
    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
    */
-}
+   let money = [12.34, 9.99]
+   const dinero = money.map ((element) => {
+  
+      return element = `$${money}`
+  
+  })
+   console.log(dinero)
 
-function returnAllTrueValues(values) {
+
+
+
+
+
   /**
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
-}
+const value = values.filter(function(happy) {
+  if(values.happy === true) {
+    return true;
+  }
+})
 
-function addTwo(nums) {
+
   /**
    * @param nums => [1,2,3,4]
    * Using .reduce, return the total sum of all numbers.
@@ -50,14 +88,31 @@ function addTwo(nums) {
    */
   //   3
   //   4 => 7
-}
 
-function joinStrings(strings) {
+  let objects = [23,42,24,55]
+  const sum = objects.reduce((a,b) => {
+    return a+b + 2
+  })
+
+
+
+
+
+
   /**
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
    */
-}
+
+  const strings= ['g','s','s'];
+  const sum = strings.reduce((a,b) => {
+    return a+b + ['2','4']
+  })
+
+
+
+
+
 
 //Bonus
 function sortObjectsByValue(objs) {
